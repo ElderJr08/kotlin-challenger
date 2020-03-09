4b87d693d302a03579d154433d589e1283e54d4b
José Elder Junior <jose.elder.f.junior@concrete.com.br>
2020-03-09 09:35:01 -0300

    First commit

---
+++ src/main/kotlin/com/br/elderjr/kotlinchallanger/KotlinChallangerApplication.kt
@@ -0,0 +1,11 @@
+package com.br.elderjr.kotlinchallanger
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinChallangerApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinChallangerApplication>(*args)
+}
---
+++ src/test/kotlin/com/br/elderjr/kotlinchallanger/KotlinChallangerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.br.elderjr.kotlinchallanger
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinChallangerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
